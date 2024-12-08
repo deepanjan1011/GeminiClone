5b32e3d2e1358ef188156b31cf787b0ab275844a
Deepanjan Pati <84163972+deepanjan1011@users.noreply.github.com>
2024-12-08 14:32:14 +0530

    changes

--- api/backend.js
+++ api/backend.js
@@ -8,5 +8,5 @@ app.get("/api/your-endpoint", (req, res) => {
   res.json({ message: "Hello from Vercel backend!" });
 });
 
-module.exports = app; // For testing locally
-module.exports.handler = serverless(app); // For Vercel
+module.exports = app; // For local testing
+module.exports.handler = serverless(app); // For Vercel deployment
